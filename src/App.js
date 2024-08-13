e626e1cc9d02e2a2169754594c3d8c8ec32d87ce
jhonatanmotta <jhonatanmotta07@gmail.com>
2024-08-13 18:26:22 -0500

    feat[project]: se ha finalizado el desarrollo del microproyecto contador de clicks en donde se introdujo los estados en los componentes y los eventListeners a demas la desestructuracion en los parametros de los componenetes funcionales

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import './App.css';
+import Boton from './componentes/Boton.js';
+import Contador from './componentes/Contador.js';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
+import { useState } from 'react';
 
 function App() {
+
+  const [numClick, setNumClicks] = useState(0)
+
+  const manejarClick = () => {
+    setNumClicks(numClick+1)
+  }
+
+  const reiniciarContador = () => {
+    setNumClicks(0)
+  }
+
   return (
-    <div className="App">
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+        className='freecodecamp-logo'
+        src={freeCodeCampLogo}
+        alt='Logo de freeCodeCamp'/>
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClicks={numClick} />
+        <Boton 
+        texto='Click'
+        botonClick={true}
+        manejarClick={manejarClick}/>
+        <Boton 
+        texto='Reiniciar'
+        botonClick={false}
+        manejarClick={reiniciarContador}/>
+      </div>
     </div>
   );
 }
